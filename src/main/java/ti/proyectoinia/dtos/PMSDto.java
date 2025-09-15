0a6be0e0816051341f00457c827da759a65e7edb
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2025-09-15 18:20:34 -0300

    Dto Structure

--- src/main/java/ti/proyectoinia/dtos/PMSDto.java
+++ src/main/java/ti/proyectoinia/dtos/PMSDto.java
@@ -1,4 +1,42 @@
 package ti.proyectoinia.dtos;
 
+import lombok.Generated;
+import lombok.Getter;
+import ti.proyectoinia.business.entities.Lote;
+
 public class PMSDto {
+    @Getter
+    private Long id;
+    @Getter
+    private Lote lote;
+    @Getter
+    private float pesoMilSemillas;
+    @Getter
+    private float humedadPorcentual;
+    @Getter
+    private float fechaMedicion;
+    @Getter
+    private String metodo;
+    @Getter
+    private String observaciones;
+    @Getter
+    private boolean activo;
+
+    @Generated
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof PMSDto)) return false;
+        PMSDto other = (PMSDto) o;
+        return id != null && id.equals(other.id);
+    }
+
+    @Generated
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Generated
+    public String toString() {
+        return "PMSDto(id=" + id + ", pesoMilSemillas=" + pesoMilSemillas + ")";
+    }
 }
--- src/main/java/ti/proyectoinia/dtos/ReciboDto.java
+++ src/main/java/ti/proyectoinia/dtos/ReciboDto.java
@@ -1,4 +1,56 @@
 package ti.proyectoinia.dtos;
 
+import lombok.Generated;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
 public class ReciboDto {
+    @Getter
+    private Long id;
+    @Getter
+    private int nroAnalisis;
+    @Getter
+    private String especie;
+    @Getter
+    private String ficha;
+    @Getter
+    private LocalDateTime fechaRecibo;
+    @Getter
+    private String remitente;
+    @Getter
+    private String origen;
+    @Getter
+    private String cultivar;
+    @Getter
+    private String deposito;
+    @Getter
+    private String estado;
+    @Getter
+    private int lote;
+    @Getter
+    private float kgLimpios;
+    @Getter
+    private String analisisSolicitados;
+    @Getter
+    private int articulo;
+    @Getter
+    private boolean activo;
+
+    @Generated
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof ReciboDto)) return false;
+        ReciboDto other = (ReciboDto) o;
+        return id != null && id.equals(other.id);
+    }
+
+    @Generated
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Generated
+    public String toString() {
+        return "ReciboDto(id=" + id + ", nroAnalisis=" + nroAnalisis + ")";
+    }
 }
