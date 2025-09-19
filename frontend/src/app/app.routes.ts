8026d78e87b85c86a25e859d4d3089c04ca54d77
renzoPeraza <renzo.peraza@estudiantes.utec.edu.uy>
2025-09-19 06:13:28 -0300

    Home y header perfectos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+// import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: 'header', component: HeaderComponent }
+    
+    { path : 'home', component: HomeComponent }
+    
 ];
