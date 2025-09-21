819125fc1dc3c6b7149a4840a36b451725276144
renzoPeraza <renzo.peraza@estudiantes.utec.edu.uy>
2025-09-21 00:22:59 -0300

    Recibo
    Recibo practicamente terminado, se le haran arreglos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 // import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { ReciboComponent } from './components/recibo/recibo.component';
 
 export const routes: Routes = [
-    
-    { path : 'home', component: HomeComponent }
-    
+
+    { path : 'home', component: HomeComponent },
+    { path : 'recibo', component: ReciboComponent }
+
 ];
