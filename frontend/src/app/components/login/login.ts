b7a30d2da01d0b56ac2af036734eba58604b736d
Felipe <164377411+felipecamejo@users.noreply.github.com>
2025-09-22 13:17:43 -0300

    listo casi todo desde styles scss

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule, CardModule, ButtonModule],
+  imports: [CommonModule, FormsModule, CardModule, ButtonModule, InputTextModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
 
