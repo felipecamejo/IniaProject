d5247e5eb13311bcaca77caa0e7c844c822fef29
Felipe <164377411+felipecamejo@users.noreply.github.com>
2025-12-07 17:51:33 -0300

    dosn arreglado

--- src/main/java/ti/proyectoinia/business/repositories/DOSNMalezaRepository.java
+++ src/main/java/ti/proyectoinia/business/repositories/DOSNMalezaRepository.java
@@ -4,6 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ti.proyectoinia.business.entities.DOSNMaleza;
 
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 @Repository
 public interface DOSNMalezaRepository extends JpaRepository<DOSNMaleza, Long> {
+	@Modifying
+	@Query("DELETE FROM DOSNMaleza dm WHERE dm.dosn.id = :dosnId")
+	void deleteByDosnId(@Param("dosnId") Long dosnId);
 }
